1f0060e77d233b17c6f9b05b10aa6ba0d7e47100
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2021-09-22 21:04:30 +0300

    Fixing redirecting for Heroku errors

--- application/app.py
+++ application/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 import routes.login
+
+@app.route("/")
+def redirect_to_login():
+    return redirect("/login")
